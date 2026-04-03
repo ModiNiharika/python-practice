b97c2217ab667c3c41863ff77e938ffb0527477a
ModiNiharika <niharikam1045@gmail.com>
2026-04-03 06:56:26 +0530

    Added file related to Pickling and unpickling --- EmpPickEx2.py

---
+++ 08_FileHandling/Pickling_UnpicklingExample/EmpPickEx2.py
@@ -0,0 +1,32 @@
+#Program for reading employee values and save them as record in file
+
+import pickle
+def saverecord():
+    with open("emp.data",'ab') as fp:
+        while(True):
+            try:
+            #Accept the employee values from keyboard
+                print('---------------------------------------')
+                eno = int(input("Enter employee Number : "))
+                ename = input("Enter the employee name : ")
+                sal = float(input("Enter employee salary : "))
+                dsg = input("Enter employee designation :")
+                print('---------------------------------------')
+                
+                #Create an empty list and place employee details
+                lst =list()
+                lst.append(eno)
+                lst.append(ename)
+                lst.append(sal)
+                lst.append(dsg)
+
+                #Save or transfer lst data into the file
+                pickle.dump(lst,fp)
+                print("Employee Record saved in file sucessfully")
+                print('---------------------------------------')
+                ch = input("Do you want to insert another employee value(Yes/No) ")
+                if(ch.lower()=='no'):
+                    print("Thanks for using this program")
+                    break
+            except ValueError:
+                print("Don't enter alnums ,strings and other data")
