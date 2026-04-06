4ba8279165d0074588339f64deee341624c085a3
ModiNiharika <niharikam1045@gmail.com>
2026-04-06 18:35:00 +0530

    Added file related to pickling and unpickling in file handling -- EmpPickUnPickMenu.py

---
+++ 08_FileHandling/Pickling_UnpicklingExample/EmpPickUnPickMenu.py
@@ -0,0 +1,9 @@
+
+def menu():
+    print('-'*50)
+    print("Employee Operations")
+    print('-'*50)
+    print('1. Save employee Data')
+    print('2. Record Employee Data')
+    print('3. Exit')
+    print('-'*50)
